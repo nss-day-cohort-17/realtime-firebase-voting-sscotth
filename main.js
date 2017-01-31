60aaaf1d20aa9f9fbd083279ce81596272820669
Scott Humphries <git@sscotth.io>
2017-01-31 15:40:46 -0600

    finish

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 {
+
   firebase.initializeApp({
     apiKey: 'AIzaSyDF235EkOk0ZBodnGkNqak5Q7Y0OxVdNr4',
     authDomain: 'c17-voting.firebaseapp.com',
@@ -7,40 +8,105 @@
     messagingSenderId: '68201900162',
   })
 
-  document
-    .querySelectorAll('.choice button')
-    .forEach(btn => btn.addEventListener('click', onVote))
+  const MAX_MESSAGES_DISPLAYED = 10
 
-  function onVote (evt) {
-    // submit the vote
-    const voteURL = 'https://c17-voting.firebaseio.com/votes.json'
+  const votesRef = firebase.database().ref('votes')
+  const messagesRef = firebase.database().ref('messages')
 
-    // // what button i clicked on
+  const form = document.querySelector('form')
+  const messagesDiv = document.querySelector('.messages')
+  const voteButtons = document.querySelectorAll('.choice button')
+  const voteTotals = document.querySelectorAll('.vote_totals')
+
+  const sendVote = (evt) => {
+    // what button i clicked on
     const voteFor = evt.target.closest('.choice').dataset.value
 
-    // // go get the current counts
-    firebase.database().ref('votes').once('value')
+    // go get the current counts
+    votesRef.once('value')
       .then(snap => snap.val())
       .then(data => {
-        // patch the new count
+        // update the new count
         const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1
-        return firebase.database().ref('votes').update({ [voteFor]: newCount })
+        votesRef.update({ [voteFor]: newCount })
       })
       .catch(console.error)
 
-    document.querySelectorAll('button').forEach(btn => btn.remove())
-    document.querySelectorAll('.hidden').forEach(item => item.classList.remove('hidden'))
+    voteButtons.forEach(btn => btn.remove())
+    voteTotals.forEach(item => item.classList.remove('hidden'))
   }
 
-  firebase.database().ref('votes').on('value', onUpdate)
+  const sendMessage = (evt) => {
+    evt.preventDefault()
 
-  function onUpdate (snap) {
-    const data = snap.val()
+    const nameInput = evt.target.elements.name // evt.target.querySelctor('[name="name"]')
+    const contentInput = evt.target.elements.content // evt.target.querySelctor('[name="content"]')
 
-    document.querySelectorAll('h3').forEach(choice => {
-      const total = Object.values(data).reduce((acc, val) => acc + val)
-      const current = data[choice.closest('.choice').dataset.value]
-      choice.innerText = Math.round(current / total * 100) + "%"
+    const name = nameInput.value.trim()
+    const content = contentInput.value.trim()
+
+    if (name && content) {
+      messagesRef.push({ name, content })
+        .then(() => contentInput.value = '')
+    }
+  }
+
+  const onNewVote = (snap) => {
+    const votes = snap.val()
+
+    document.querySelectorAll('h3').forEach(voteTotal => {
+      const choice = voteTotal.closest('.choice').dataset.value
+      const total = Object.values(votes).reduce((acc, val) => acc + val) // SUM
+      const current = votes[choice]
+
+      voteTotal.innerText = Math.round(current / total * 100) + '%'
     })
   }
+
+  const renderMessage = (msg) => {
+    const docFragment = document.createDocumentFragment()
+
+    const div = document.createElement('DIV')
+    docFragment.appendChild(div)
+
+    const strong = document.createElement('STRONG')
+    div.appendChild(strong)
+
+    const name = document.createTextNode(msg.name)
+    strong.appendChild(name)
+
+    const seperator = document.createTextNode(': ')
+    div.appendChild(seperator)
+
+    const span = document.createElement('SPAN')
+    div.appendChild(span)
+
+    const content = document.createTextNode(msg.content)
+    span.appendChild(content)
+
+    return docFragment
+  }
+
+  const onNewMessage = (snap) => {
+    const msg = snap.val() // { name: 'Scott', content: 'Hey' }
+
+    messagesDiv.appendChild(renderMessage(msg))
+
+    // messagesDiv.innerHTML += `
+    //   <div>
+    //     <strong>${msg.name}</strong>: <span>${msg.content}</span>
+    //   </div>
+    // `
+
+    if (messagesDiv.childElementCount > MAX_MESSAGES_DISPLAYED) {
+      messagesDiv.firstChild.remove()
+    }
+  }
+
+  form.addEventListener('submit', sendMessage)
+  voteButtons.forEach(btn => btn.addEventListener('click', sendVote))
+
+  votesRef.on('value', onNewVote)
+  messagesRef.limitToLast(MAX_MESSAGES_DISPLAYED).on('child_added', onNewMessage)
+
 }
