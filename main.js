548fcf7211236ca1f14aa5a78618d6eaa1b25615
Scott Humphries <git@sscotth.io>
2017-01-31 12:05:52 -0600

    use firebase database JS api

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyDF235EkOk0ZBodnGkNqak5Q7Y0OxVdNr4',
+    authDomain: 'c17-voting.firebaseapp.com',
+    databaseURL: 'https://c17-voting.firebaseio.com',
+    storageBucket: 'c17-voting.appspot.com',
+    messagingSenderId: '68201900162',
+  })
+
   document
     .querySelectorAll('.choice button')
     .forEach(btn => btn.addEventListener('click', onVote))
@@ -11,16 +19,19 @@
     const voteFor = evt.target.closest('.choice').dataset.value
 
     // // go get the current counts
-    fetch(voteURL)
-      .then(res => res.json())
+    // fetch(voteURL)
+    //   .then(res => res.json())
+    firebase.database().ref('votes').once('value')
+      .then(snap => snap.val())
       .then(data => {
         // patch the new count
         const newCount = data && data[voteFor] ? data[voteFor] += 1 : 1
 
-        return fetch(voteURL, {
-          method: 'PATCH',
-          body: JSON.stringify({ [voteFor]: newCount })
-        })
+        // return fetch(voteURL, {
+        //   method: 'PATCH',
+        //   body: JSON.stringify({ [voteFor]: newCount })
+        // })
+        return firebase.database().ref('votes').update({ [voteFor]: newCount })
         .then(() => {
           document.querySelectorAll('h3').forEach(choice => {
             const total = Object.values(data).reduce((acc, val) => acc + val)
